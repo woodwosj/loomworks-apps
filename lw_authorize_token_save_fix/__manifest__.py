938da804718b1d1397499c89952af6a99b6e9616
Loomworks Solutions <apps@loomworks.solutions>
2026-08-26 19:36:56 -0400

    assets: lw_authorize_token_save_fix hero art + flow diagram

--- lw_authorize_token_save_fix/__manifest__.py
+++ lw_authorize_token_save_fix/__manifest__.py
@@ -34,7 +34,7 @@ void in ``payment``/``payment_authorize``.
     'support': 'apps@loomworks.solutions',
     'license': 'LGPL-3',
     'price': 0,
-    'images': ['static/description/screenshot_1.png', 'static/description/screenshot_2.png', 'static/description/screenshot_3.png', 'static/description/screenshot_4.png'],
+    'images': ['static/description/screenshot_1.png', 'static/description/screenshot_2.png', 'static/description/screenshot_3.png', 'static/description/screenshot_4.png', 'static/description/hero.png', 'static/description/flow.png'],
     'depends': ['payment_authorize'],
     'installable': True,
     'application': False,
